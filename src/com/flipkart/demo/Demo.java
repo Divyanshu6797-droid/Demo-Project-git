1b12cad16ce01a916035395f78c68572b17620f0
Divyanshu6797-droid <divyanshu.kumar4@flipkart.com>
2024-08-06 16:13:05 +0530

    STS

---
+++ src/com/flipkart/demo/Demo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.flipkart.demo;
+
+/**
+ * 
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("its my STS project");
+		
+
+	}
+
+}
